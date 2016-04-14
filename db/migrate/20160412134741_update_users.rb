e437fcccec518a559f2d464c3ed2fd05333b1dfd
“DonalMoo” <donalmooney87@gmail.com>
2016-04-14 17:26:28 +0100

    updated migrations3

--- db/migrate/20160412134741_update_users.rb
+++
@@ -1,8 +0,0 @@
-#class UpdateUsers < ActiveRecord::Migration
-  #def change
-  	#@u = User.find_by( email: 'admin@email.ie')
-  	 #if @u != nil
-  	 #			@u.update_attribute :admin, true
-  	 #		end
- # end
-#end

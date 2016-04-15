7283642671824c6268b7912912230be96cf17ad4
“DonalMoo” <donalmooney87@gmail.com>
2016-04-15 12:58:06 +0100

    changed added admin

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160414093954) do
+ActiveRecord::Schema.define(version: 20160415114453) do
 
   create_table "bikes", force: :cascade do |t|
     t.string   "description"
